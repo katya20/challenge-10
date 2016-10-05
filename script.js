aecc77f4e2431c0fad7adfb717ba56d0499ed685
katya20 <kpanchenko21@gmail.com>
2016-10-05 08:44:30 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var input = $("new-item-input")
 input.keydown(function(e) {	
   
   if (e.keyCode == "13") {
-      var valueToSave = input.val();
+    var valueToSave = input.val();
     addListItem(valueToSave);
   }
 })
